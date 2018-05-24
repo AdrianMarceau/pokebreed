2b0965cb6499c4390c8d436e9ff376d4a2a32a96
AdrianMarceau <adrian.marceau@gmail.com>
2018-05-23 21:37:58 -0400

    Tweaked text on About tab

--- pages/about.php
+++ pages/about.php
@@ -1,25 +1,24 @@
 <div>
     <div class="group">
         <h2>What is the PokéBox Simulator?</h2>
-        <p>Add up to 10 different Pokémon to the zone and then watch as they grow, evolve, multiply, and eventually faint leaving their offspring to continue the cycle.</p>
-        <p>The kinds of Pokémon you add to a zone can alter it's appeal to other species and can either positively or negatively affect their growth and breeding behaviors over time.</p>
-        <p>Pokémon will typically only breed with their own species, but adding a single Ditto to your zone at the beginning can give every species a head-start on egg production.</p>
-        <p>This is a simulator and not an actual game, so you're free to play however you want and come up with your own goals. Just relax and let the Pokémon do their thing.</p>
+        <p>
+            Add up to 10 different Pokémon to the zone and then watch as they grow, evolve, multiply, and
+            eventually faint leaving their offspring to continue the cycle. The kinds of Pokémon you add to a
+            zone can alter it's appeal to other species and can either positively or negatively affect their
+            growth and breeding behaviors over time.
+        </p>
+        <p>
+            Pokémon will typically only breed with their own species, but adding a single Ditto to your zone at
+            the beginning can give every species a head-start on egg production. This is a simulator and not an
+            actual game, so you're free to play however you want and come up with your own goals. Just relax
+            and let the Pokémon do their thing.
+        </p>
     </div>
     <div class="group">
         <h2>Available Modes</h2>
         <ul>
             <li><a href="/">Normal Mode</a> - Complete your Pokedex! Use starter Pokémon to attract new species, then chain to find them all!</li>
             <li><a href="free-mode/">Free Mode</a> - No Limits! Any basic Pokémon can be added to your box, but records are not saved.</li>
-        </ul>
-    </div>
-    <div class="group">
-        <h2>Future Goals for the Project</h2>
-        <ul>
-            <li>Allow the user to start the simulator with less than 10 Pokémon if they want to</li>
-            <li>Keep track of which species the player has seen so-far and have it persistant between sessions, display somewhere</li>
-            <li>Revise breeding so that gender ratios and egg groups can be respected so that Pokémon like Jynx and Hitmonlee can breed and function</li>
-            <li>Program a predator/prey relationship so that the more perasive species (looking at you, Magikarp) can be kept in check by stronger ones</li>
         </ul>
     </div>
 </div>
