85a16b6d02482631f23f12d4bd7a321433217996
AdrianMarceau <adrian.marceau@gmail.com>
2018-05-25 01:21:27 -0400

    Version 0.9.12

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 // Define the global cache time and version number
 $last_updated = '2018-05-25';
-$version_number = '0.9.11';
+$version_number = '0.9.12';
 
 // If requested, print out the version number and exit
 if (!empty($_GET['return'])){
