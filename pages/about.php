6e75d181d8d87729284e02c3b960bf77e3c2a8fc
AdrianMarceau <adrian.marceau@gmail.com>
2018-05-25 01:14:05 -0400

    Removed a word from the about page

--- pages/about.php
+++ pages/about.php
@@ -2,7 +2,7 @@
     <div class="group">
         <h2>What is the PokéBox Simulator?</h2>
         <p>
-            PokéBox is a browser-based simulator / game that revolves around the biomes, ecosystems,
+            PokéBox is a browser-based simulator that revolves around the biomes, ecosystems,
             relationships, and lifecycles of Pokémon. The basic goal of the simulator is to encounter
             <em>all</em> the different species and forms of Pokémon using a only single box and a handful
             of starter Pokémon as seeds. Your starter Pokémon can be used to attract other similar species,
