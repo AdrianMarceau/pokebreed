2b88d6c93521a24f4143c7fa976ab7bfbf050bd8
AdrianMarceau <adrian.marceau@gmail.com>
2018-05-27 03:37:45 -0400

    Removed filler text from Pokedex page

--- pages/pokedex.php
+++ pages/pokedex.php
@@ -1,13 +1,6 @@
 <div>
-    <div class="group">
-        <h2>PokéBox Pokédex</h2>
-        <p>
-            Complete your Pokédex by encountering different Pokemon in the simulator.  Use your existing species
-            strategically and you should be able to lure in new ones, then from there use the new ones to attract
-            even more! Eventually you've have encountered them all!
-        </p>
-    </div>
     <div class="group pokedex">
+        <h2>PokéBox Pokédex</h2>
         <ul class="list"></ul>
     </div>
 </div>
