b4a20d7e8ae3cf4da146028e55f87eb450c51c15
AdrianMarceau <adrian.marceau@gmail.com>
2018-06-02 06:03:10 -0400

    Removed forced pause on Pokedex open

--- scripts/script.js
+++ scripts/script.js
@@ -222,7 +222,7 @@
             if (appFreeMode){ return false; }
             var $thisLink = $(this);
             //var currentSpeed = $('body').attr('data-speed');
-            $controlButtons.filter('.pause').trigger('click');
+            //$controlButtons.filter('.pause').trigger('click');
             var isShowing = $panelDiv.attr('data-view') === 'pokedex' ? true : false;
             if (!isShowing){
                 // Show it
