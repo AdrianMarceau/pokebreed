4d056778b6717143c46ca7c0bb2cedd8d3c51c58
AdrianMarceau <adrian.marceau@gmail.com>
2020-01-04 19:40:33 -0500

    Added data + images for Galarian Farfetch'd line

--- data/indexes/species/gen8.js
+++ data/indexes/species/gen8.js
@@ -11,6 +11,143 @@
     thisIndex.setGeneration(8);
     thisIndex.setRegion('galar');
 
+    // Add the generation's list of regional variants to the index
+    var indexOrder = thisIndex.indexOrder;
+    thisIndex.addSpeciesIndex({
+
+        // Galarian Variants
+
+        'galarian-meowth': {
+            order: thisIndex.afterOrder('persian', 5),
+            token: 'galarian-meowth',
+            name: 'Galarian Meowth',
+            formClass: 'regional-variant',
+            formToken: 'galarian',
+            baseSpecies: 'meowth',
+            number: 52,
+            types: ['steel'],
+            baseStats: {hp: 50, phAttack: 65, phDefense: 55, spAttack: 40, spDefense: 40, speed: 40},
+            abilities: {0: 'pickup', 1: 'tough-claws', hidden: 'unnerve'},
+            height: 0.4,
+            weight: 7.5,
+            colors: ['brown', 'black', 'yellow', 'white'],
+            eggCycles: 20,
+            eggGroups: ['field'],
+            nextEvolutions: [{
+                species: 'perrserker',
+                method: 'level-up',
+                value: 28
+                }],
+            altBaseEvolutions: [{
+                species: 'meowth',
+                method: 'type-warning',
+                value: 'steel',
+                method2: 'type-warning',
+                value2: 'dark'
+                },{
+                species: 'alolan-meowth',
+                method: 'type-warning',
+                value: 'steel',
+                method2: 'type-warning',
+                value2: 'normal'
+                },{
+                species: 'meowth',
+                method: 'type-appeal',
+                value: 'normal'
+                },{
+                species: 'alolan-meowth',
+                method: 'type-appeal',
+                value: 'dark'
+                }]
+            },
+
+        'galarian-ponyta': {
+            order: thisIndex.afterOrder('rapidash', 1),
+            token: 'galarian-ponyta',
+            name: 'Galarian Ponyta',
+            formClass: 'regional-variant',
+            formToken: 'galarian',
+            baseSpecies: 'ponyta',
+            number: 77,
+            types: ['psychic'],
+            baseStats: {hp: 50, phAttack: 85, phDefense: 55, spAttack: 65, spDefense: 65, speed: 90},
+            abilities: {0: 'run-away', 1: 'pastel-veil', hidden: 'anticipation'},
+            height: 0.8,
+            weight: 24.0,
+            colors: ['white', 'pink', 'blue'],
+            eggCycles: 20,
+            eggGroups: ['field'],
+            nextEvolutions: [{
+                species: 'galarian-rapidash',
+                method: 'level-up',
+                value: 40
+                }],
+            altBaseEvolutions: [{
+                species: 'ponyta',
+                method: 'type-warning',
+                value: 'psychic'
+                }]
+            },
+        'galarian-rapidash': {
+            order: thisIndex.afterOrder('rapidash', 2),
+            token: 'galarian-rapidash',
+            name: 'Galarian Rapidash',
+            formClass: 'regional-variant',
+            formToken: 'galarian',
+            baseSpecies: 'rapidash',
+            number: 78,
+            types: ['psychic', 'fairy'],
+            baseStats: {hp: 65, phAttack: 100, phDefense: 70, spAttack: 80, spDefense: 80, speed: 105},
+            abilities: {0: 'run-away', 1: 'pastel-veil', hidden: 'anticipation'},
+            height: 1.7,
+            weight: 80.0,
+            colors: ['white', 'pink', 'blue', 'black'],
+            eggCycles: 20,
+            eggGroups: ['field'],
+            prevEvolution: 'galarian-ponyta'
+            },
+
+        'galarian-farfetchd': {
+            order: thisIndex.afterOrder('farfetchd', 1),
+            token: 'galarian-farfetchd',
+            name: 'Galarian Farfetch\'d',
+            formClass: 'regional-variant',
+            formToken: 'galarian',
+            baseSpecies: 'farfetchd',
+            number: 83,
+            types: ['fighting'],
+            baseStats: {hp: 52, phAttack: 95, phDefense: 55, spAttack: 58, spDefense: 62, speed: 55},
+            abilities: {0: 'steadfast', hidden: 'scrappy'},
+            height: 0.8,
+            weight: 42.0,
+            colors: ['brown', 'white', 'green', 'yellow'],
+            eggCycles: 20,
+            eggGroups: ['flying', 'field'],
+            nextEvolutions: [{
+                species: 'sirfetchd',
+                method: 'level-up',
+                value: 33,
+                method2: 'royal-ascension',
+                value2: 33
+                }],
+            altBaseEvolutions: [{
+                species: 'farfetchd',
+                method: 'type-warning',
+                value: 'fighting'
+                },{
+                species: 'farfetchd',
+                method: 'type-appeal',
+                value: 'normal'
+                },{
+                species: 'farfetchd',
+                method: 'type-crisis',
+                value: 'grass'
+                }]
+            },
+
+    });
+    thisIndex.indexOrder = indexOrder;
+
     // Add the generation's list of base species to the index
     var indexOrder = thisIndex.indexOrder;
     thisIndex.addSpeciesIndex({
@@ -29,17 +166,33 @@
             height: 0.8,
             weight: 28.0,
             colors: ['gray', 'brown', 'black', 'yellow', 'white'],
-            evoLevel: 28,
             eggCycles: 20,
             eggGroups: ['field'],
             prevEvolution: 'galarian-meowth'
+            },
+
+        'sirfetchd': {
+            order: thisIndex.afterOrder('farfetchd', 2),
+            token: 'sirfetchd',
+            name: 'Sirfetch\'d',
+            number: 865,
+            baseGameGeneration: 1,
+            types: ['fighting'],
+            baseStats: {hp: 62, phAttack: 135, phDefense: 95, spAttack: 68, spDefense: 82, speed: 65},
+            abilities: {0: 'steadfast', hidden: 'scrappy'},
+            height: 0.8,
+            weight: 117.0,
+            colors: ['white', 'green', 'yellow', 'black'],
+            eggCycles: 20,
+            eggGroups: ['flying', 'field'],
+            prevEvolution: 'galarian-farfetchd'
             },
 
 
         });
     thisIndex.indexOrder = indexOrder;
 
-    // Add the generation's list of extra species to the index
+    // Add the generation's list of burst/mega/gmax evolutions to the index
     var indexOrder = thisIndex.indexOrder;
     thisIndex.addSpeciesIndex({
 
@@ -305,103 +458,13 @@
             hasBigSprite: true
             },
 
-        // Galarian Variants
-
-        'galarian-meowth': {
-            order: thisIndex.afterOrder('persian', 5),
-            token: 'galarian-meowth',
-            name: 'Galarian Meowth',
-            formClass: 'regional-variant',
-            formToken: 'galarian',
-            baseSpecies: 'meowth',
-            number: 52,
-            types: ['steel'],
-            baseStats: {hp: 50, phAttack: 65, phDefense: 55, spAttack: 40, spDefense: 40, speed: 40},
-            abilities: {0: 'pickup', 1: 'tough-claws', hidden: 'unnerve'},
-            height: 0.4,
-            weight: 7.5,
-            colors: ['brown', 'black', 'yellow', 'white'],
-            eggCycles: 20,
-            eggGroups: ['field'],
-            nextEvolutions: [{
-                species: 'perrserker',
-                method: 'level-up',
-                value: 28
-                }],
-            altBaseEvolutions: [{
-                species: 'meowth',
-                method: 'type-warning',
-                value: 'steel',
-                method2: 'type-warning',
-                value2: 'dark'
-                },{
-                species: 'alolan-meowth',
-                method: 'type-warning',
-                value: 'steel',
-                method2: 'type-warning',
-                value2: 'normal'
-                },{
-                species: 'meowth',
-                method: 'type-appeal',
-                value: 'normal'
-                },{
-                species: 'alolan-meowth',
-                method: 'type-appeal',
-                value: 'dark'
-                }]
-            },
-
-        'galarian-ponyta': {
-            order: thisIndex.afterOrder('rapidash', 1),
-            token: 'galarian-ponyta',
-            name: 'Galarian Ponyta',
-            formClass: 'regional-variant',
-            formToken: 'galarian',
-            baseSpecies: 'ponyta',
-            number: 77,
-            types: ['psychic'],
-            baseStats: {hp: 50, phAttack: 85, phDefense: 55, spAttack: 65, spDefense: 65, speed: 90},
-            abilities: {0: 'run-away', 1: 'pastel-veil', hidden: 'anticipation'},
-            height: 0.8,
-            weight: 24.0,
-            colors: ['white', 'pink', 'blue'],
-            eggCycles: 20,
-            eggGroups: ['field'],
-            nextEvolutions: [{
-                species: 'galarian-rapidash',
-                method: 'level-up',
-                value: 40
-                }],
-            altBaseEvolutions: [{
-                species: 'ponyta',
-                method: 'type-warning',
-                value: 'psychic'
-                }]
-            },
-        'galarian-rapidash': {
-            order: thisIndex.afterOrder('rapidash', 2),
-            token: 'galarian-rapidash',
-            name: 'Galarian Rapidash',
-            formClass: 'regional-variant',
-            formToken: 'galarian',
-            baseSpecies: 'rapidash',
-            number: 78,
-            types: ['psychic', 'fairy'],
-            baseStats: {hp: 65, phAttack: 100, phDefense: 70, spAttack: 80, spDefense: 80, speed: 105},
-            abilities: {0: 'run-away', 1: 'pastel-veil', hidden: 'anticipation'},
-            height: 1.7,
-            weight: 80.0,
-            colors: ['white', 'pink', 'blue', 'black'],
-            eggCycles: 20,
-            eggGroups: ['field'],
-            prevEvolution: 'galarian-ponyta'
-            },
-
     });
     thisIndex.indexOrder = indexOrder;
 
     // Update previous gen pokemon with new next evolution data
     thisIndex.addNextEvolutions([
+
+        // Gigantamax Forms
 
         {base: 'charizard', species: 'gmax-charizard', method: 'burst-evolution', value: 'dynamax-energy', method2: 'gigantamax-factor', value2: 100},
         {base: 'butterfree', species: 'gmax-butterfree', method: 'burst-evolution', value: 'dynamax-energy', method2: 'gigantamax-factor', value2: 100},
@@ -421,14 +484,19 @@
     // Update previous gen pokemon with alt base evolution data
     thisIndex.addAltBaseEvolutions([
 
+        // Regional Forms
+
         {base: 'meowth', species: 'galarian-meowth', method: 'type-warning', value: 'normal', method2: 'type-warning', value2: 'dark'},
         {base: 'meowth', species: 'galarian-meowth', method: 'type-appeal', value: 'steel'},
-
         {base: 'alolan-meowth', species: 'galarian-meowth', method: 'type-warning', value: 'dark', method2: 'type-warning', value2: 'normal'},
         {base: 'alolan-meowth', species: 'galarian-meowth', method: 'type-appeal', value: 'steel'},
 
         {base: 'ponyta', species: 'galarian-ponyta', method: 'type-warning', value: 'fire'},
         {base: 'ponyta', species: 'galarian-ponyta', method: 'type-appeal', value: 'psychic'},
+
+        {base: 'farfetchd', species: 'galarian-farfetchd', method: 'type-warning', value: 'normal'},
+        {base: 'farfetchd', species: 'galarian-farfetchd', method: 'type-appeal', value: 'fighting'},
+        {base: 'farfetchd', species: 'galarian-farfetchd', method: 'type-surge', value: 'grass'},
 
         ]);
 
