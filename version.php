331e36997f57bda4a7d5edb142c55afd4bd669ed
AdrianMarceau <adrian.marceau@gmail.com>
2020-03-22 00:30:44 -0400

    Version 0.17.1

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?
 
 // Define the global cache time and version number
-$last_updated = '2020-03-20';
-$version_number = '0.17.0';
+$last_updated = '2020-03-22';
+$version_number = '0.17.1';
 
 // If requested, print out the version number and exit
 if (!empty($_GET['return'])){
