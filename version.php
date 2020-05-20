3cf7e46508330e7fa44d846f32bfc56b89ddb103
AdrianMarceau <adrian.marceau@gmail.com>
2020-05-19 21:04:13 -0400

    Version 1.0.1

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?
 
 // Define the global cache time and version number
-$last_updated = '2020-05-18';
-$version_number = '1.0.0';
+$last_updated = '2020-05-19';
+$version_number = '1.0.1';
 
 // Set the access control headers to allow others to use it
 header("Access-Control-Allow-Origin: *");
